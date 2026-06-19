18ec5b167334857e24f7217117830663ae0a3581
mohamvazeer-cell <mohamvazeer@gmail.com>
2026-06-19 02:35:09 +0100

    Initialize Flask app with a home route

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello from my project")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, this is my cloud-based CI/CD Flask app!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
